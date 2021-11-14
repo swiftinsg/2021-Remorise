7cf2c4318a73c956baff36eecc19779514572433
Trinity <trinitylee08@rafflesgirlssch.edu.sg>
2021-11-14 19:08:25 +0800

    Initial Commit

---
+++ Remorise/RemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoriseApp.swift
+//  Remorise
+//
+//  Created by Trinity Lee on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
