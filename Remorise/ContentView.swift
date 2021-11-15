4b7dc27f6146516188b45a4287149f16d2d34907
wenyue <wenyue.gao@gmail.com>
2021-11-15 15:19:27 +0800

    completed home screen elements (alignment not completed yet)

--- Remorise/ContentView.swift
+++ Remorise/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            HStack {
+                Text("Welcome, esteemed learner!")
+                Image("hi")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .scaledToFit()
+                    .aspectRatio( contentMode: ContentMode.fit)
+                    .frame(width: 30, height: 30)
+            }
+            HStack {
+                ZStack {
+                    RoundedRectangle (cornerRadius: 30)
+                        .fill(Color.blue)
+                        .frame(width: 40, height: 40, alignment: .top
+                    )
+                    Text("All")
+                }
+                
+                .padding(0.0)
+                
+            }
+            VStack {
+                Text("No flashcards created yet")
+            }
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+}
+
