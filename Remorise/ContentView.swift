4b146c0e996e451c7beb3c685a8819881145bc87
wenyue <wenyue.gao@gmail.com>
2021-11-19 09:54:15 +0800

    linkages of home screen, create new flashcard and new tag sheet done

--- Remorise/ContentView.swift
+++ Remorise/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var isFlashcardPresented = false
     var body: some View {
         VStack {
             HStack {
@@ -26,58 +27,61 @@ struct ContentView: View {
                 
                 
                 Button {
-                    
+                    isFlashcardPresented = true
                 } label: {
                     Image(systemName: "plus")
                         .foregroundColor(Color("Oxford Blue"))
                         .font(.system(size: 30))
                 }
                 .padding()
-                .frame(width: 50, height: 50)
+                .frame(width: 50, height: 40)
                 .background(Color("Beau Blue"))
                 .clipShape(Circle())
                 
             }
-   
+            
             VStack(alignment: .leading) {
                 ScrollView(.horizontal) {
-                Button {
-                    
-                } label: {
-                    Text("All")
-                        .foregroundColor(Color("Oxford Blue"))
-                        .font(.system(size: 20))
+                    Button {
+                        
+                    } label: {
+                        Text("All")
+                            .foregroundColor(Color("Oxford Blue"))
+                            .font(.system(size: 20))
+                        
+                    }
+                    .padding()
+                    .frame(alignment: .leading)
+                    .background(Color("Beau Blue"))
+                    .cornerRadius(30)
+                }
+                .padding()
                 
-                }
-                .padding()
-                .frame(alignment: .leading)
-                .background(Color("Beau Blue"))
-                .cornerRadius(30)
-                }
-                .padding()
-            
                 
             }
-       
+            
             
             VStack(alignment: .center) {
                 Text("No flashcard stacks created yet, click the + button to start!")
-                        .font(.system(size: 23, design: .rounded))
-                        .frame(height: 200)
-                        .multilineTextAlignment(.center)
+                    .font(.system(size: 23, design: .rounded))
+                    .frame(height: 200)
+                    .multilineTextAlignment(.center)
                     .padding()
             }
             Spacer()
+                .sheet(isPresented: $isFlashcardPresented) {
+                    CreateFlashcardSheet()
+                    
+                }
         }
         
     }
 }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
 
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
